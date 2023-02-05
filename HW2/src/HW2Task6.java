5671672dbb0f229787dd795c5ac5445413e59c41
AliakseiShylak <shilak-a@rambler.ru>
2023-02-06 00:56:01 +0300

    Added solution of task 6

--- HW2/src/HW2Task6.java
+++ HW2/src/HW2Task6.java
@@ -3,9 +3,6 @@
  * Необходимо найти самое длинное слово в строке.
  */
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class HW2Task6 {
     public static void main(String[] args) {
         String str = "ррооd fgЩыhj, hdg rrj.";
@@ -18,7 +15,7 @@ public class HW2Task6 {
             System.out.println("No words");
             return;
         }
-        
+
         for (int i = 0; i < word.length; i++) {
             if (word[i].length() > max) {
                 max = word[i].length();
